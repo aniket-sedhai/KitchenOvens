4183ca3e51588dae2742dc8a42d251634433864b
Aniket Sedhai <asedhai@masonite.com>
2024-06-17 19:28:22 -0500

    Initial Commit

---
+++ KitchenOvens/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KitchenOvens
+//
+//  Created by Aniket Sedhai on 6/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
